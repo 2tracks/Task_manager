2292499b30e6619627174ae76b8b98aba3bd0a84
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-04-21 17:09:41 +0000

    Added insert task

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def get_task():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_task'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
