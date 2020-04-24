7b901656960fd4150894ffec70a0b750606015d7
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-04-24 09:20:03 +0000

    changes for heroku in app.py

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def add_category():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', 0.0.0.0),
-            port=int(os.environ.get('PORT', "50000")))
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')))
